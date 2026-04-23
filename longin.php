e1f217c9ac3136c8e1df701de0cf64511f404cf6
ASMA GHOBNI <ghobni202@gmail.com>
2026-04-23 22:06:11 +0100

    Create longin.php

---
+++ longin.php
@@ -0,0 +1,5 @@
+<form method="POST">
+    <input type="email" name="email" placeholder="email"><br>
+    <input type="password" name="password" placeholder="password"><br>
+    <button>Login</button>
+</form>
